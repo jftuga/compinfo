5191f379be5dfbd0b36378cf3ce342a0318db3a8
John Taylor <johntaylor@gmail.com>
2018-04-07 15:30:41 -0400

    Remove Start.xaml and .xaml.cs; code is now in MainWindow.xaml and .xaml.cs; also changed icon

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Display basic computer info
 
 ![compinfo output](compinfo/compinfo_screenshot.png) 
+
+____
+
+Icon source: http://www.iconarchive.com/show/origami-colored-pencil-icons-by-double-j-design/red-laptop-icon.html
--- compinfo/MainWindow.xaml.cs
+++ compinfo/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace compinfo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string displayUserName { get; set; }
+        public static string displayComputerName { get; set; }
+        public static string displayOS { get; set; }
+        public static string displaySerial { get; set; }
+        public static string displayUptime { get; set; }
+        public static string displayCPU { get; set; }
+        public static string displayMemory { get; set; }
+        public static string displayIPv4 { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            CompInfoGrid.DataContext = this;
+            ShowCompInfo();
+        }
+
+        public void ShowCompInfo()
+        {
+            var c = new Computer();
+
+            displayUserName = c.GetUserName;
+            displayComputerName = c.GetComputerName;
+            displayOS = c.GetOS;
+            displaySerial = c.GetSerial;
+            displayUptime = c.GetUptime;
+            displayCPU = c.GetCPU; // this is slow
+            displayMemory = c.GetMemory;
+            displayIPv4 = c.GetIPv4;
+        }
+
+        private void Click_Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
         }
     }
 }
