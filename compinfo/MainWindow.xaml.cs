18718c001a97497aa138c063398dfbafaf52034d
John Taylor <johntaylor@gmail.com>
2018-04-09 19:14:24 -0400

    Bind to view model directly instead of using properties

--- compinfo/MainWindow.xaml.cs
+++ compinfo/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace compinfo
 {
@@ -20,23 +7,10 @@ namespace compinfo
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string displayUserName => c.GetUserName;
-        public string displayComputerName => c.GetComputerName;
-        public string displayOS => c.GetOS;
-        public string displayModel => c.GetModel;
-        public string displaySerial => c.GetSerial;
-        public string displayUptime => c.GetUptime;
-        public string displayCPU => c.GetCPU;
-        public string displayMemory => c.GetMemory;
-        public string displayGraphics => c.GetGraphics;
-        public string displayIPv4 => c.GetIPv4;
-
-        private Computer c = new Computer();
-
         public MainWindow()
         {
             InitializeComponent();
-            CompInfoGrid.DataContext = this;
+            CompInfoGrid.DataContext = new Computer();
         }
 
         private void Click_Close(object sender, RoutedEventArgs e)
